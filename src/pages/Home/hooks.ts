8d0a81f0e812e879b31139ff634a380b3ad3b6e9
François Lefebvre <francois.lfe@gmail.com>
2022-01-25 20:50:19 +0100

    wip

--- src/pages/Home/hooks.ts
+++ src/pages/Home/hooks.ts
@@ -9,33 +9,33 @@ import {
 export const useTorrents = () => {
   const { data, subscribeToMore } = useTorrentsQuery();
 
-  useEffect(() => {
-    subscribeToMore<TorrentsUpdatesSubscription>({
-      document: TorrentsUpdatesDocument,
-      updateQuery: (prev, { subscriptionData }) => {
-        const { data } = subscriptionData;
-        if (!data) return prev;
+  // useEffect(() => {
+  //   subscribeToMore<TorrentsUpdatesSubscription>({
+  //     document: TorrentsUpdatesDocument,
+  //     updateQuery: (prev, { subscriptionData }) => {
+  //       const { data } = subscriptionData;
+  //       if (!data) return prev;
 
-        const { torrents } = prev;
-        const { torrentsUpdate } = data;
-        return {
-          ...prev,
-          torrents: torrents.map((torrent) => {
-            const isMatchingUpdate = torrentsUpdate.find(
-              (t) => t.torrentId === torrent.torrentId
-            );
-            if (isMatchingUpdate) {
-              return {
-                ...torrent,
-                ...isMatchingUpdate,
-              };
-            }
-            return torrent;
-          }, []),
-        };
-      },
-    });
-  }, [subscribeToMore]);
+  //       const { torrents } = prev;
+  //       const { torrentsUpdate } = data;
+  //       return {
+  //         ...prev,
+  //         torrents: torrents.map((torrent) => {
+  //           const isMatchingUpdate = torrentsUpdate.find(
+  //             (t) => t.torrentId === torrent.torrentId
+  //           );
+  //           if (isMatchingUpdate) {
+  //             return {
+  //               ...torrent,
+  //               ...isMatchingUpdate,
+  //             };
+  //           }
+  //           return torrent;
+  //         }, []),
+  //       };
+  //     },
+  //   });
+  // }, [subscribeToMore]);
 
   return {
     torrents: data?.torrents || [],
