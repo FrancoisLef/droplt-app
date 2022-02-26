d78b0bd0920389841d3fa93182ecea10b1268515
François Lefebvre <francois.lfe@gmail.com>
2022-02-26 22:49:27 +0100

    feat(wip): wip

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,25 @@
-import { Box } from '@chakra-ui/react';
+import { Box, SimpleGrid } from '@chakra-ui/react';
 
 import NameFilter from './components/NameFilter';
-// import StatCard from './components/StatCard';
+import StatCard from './components/StatCard';
 import TorrentTable from './components/TorrentTable';
 import { useTorrentsTable } from './hooks';
 
 const HomePage: React.FC = () => {
-  const { data, filter, initialState, setFilter, ...tableProps } =
-    useTorrentsTable();
+  const {
+    data,
+    filter,
+    torrentsCount,
+    initialState,
+    setFilter,
+    ...tableProps
+  } = useTorrentsTable();
 
   return (
     <Box width="full">
-      {/* <SimpleGrid mb="6" columns={{ base: 1, md: 4 }} gap={{ base: 6 }}>
-        <StatCard label="Torrents" value={data?.torrents?.length} />
-      </SimpleGrid> */}
+      <SimpleGrid mb="6" columns={{ base: 1, md: 4 }} gap={{ base: 6 }}>
+        <StatCard label="Torrents" value={torrentsCount} />
+      </SimpleGrid>
       <Box bg="white" padding="6" rounded="lg">
         <NameFilter count={data.length} value={filter} onChange={setFilter} />
         {data ? (
