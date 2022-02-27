df703a520029f4320515ee3d863903c3d4859347
François Lefebvre <francois.lfe@gmail.com>
2022-02-27 18:23:43 +0100

    feat(wip): wip

--- src/pages/Home/components/TorrentTable/components/NameFilter/locales.ts
+++
@@ -1,9 +0,0 @@
-import LocalizedStrings from 'react-localization';
-
-const locales = new LocalizedStrings({
-  fr: {
-    searchPlaceholder: 'Chercher un torrent par son nom…',
-  },
-});
-
-export default locales;
