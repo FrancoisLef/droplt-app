ab68ff2b935500112e0dab832c6174fa9e04953d
François Lefebvre <francois.lfe@gmail.com>
2022-03-31 22:36:45 +0200

    feat(wip): wip

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,22 @@ import '@fontsource/roboto-mono';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import theme from './theme';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ColorModeScript />
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
